b1bae9d37ef1de05fd2d2be800500fe8a64614eb
sabrina <sabrinaahmednitu@gmail.com>
2023-07-28 01:04:51 +0600

    platinum fetched

--- src/component/Jewellary/Platinum/Platinums.jsx
+++ src/component/Jewellary/Platinum/Platinums.jsx
@@ -1,13 +1,20 @@
+import { useEffect, useState } from 'react';
 import AllJewellaryItems from '../../../Hooks/AllJewellaryItems';
 import Platinum from './Platinum';
 
 const Platinums = () => {
-  const [golds] = AllJewellaryItems();
+     const [jewellary, setJewellary] = useState([]);
+
+     useEffect(() => {
+       fetch('http://localhost:5000/golds')
+         .then((res) => res.json())
+         .then((data) => setJewellary(data[0].platinum));
+     }, []);
   return (
     <div className="container mx-auto">
-      <h1>{golds.length} platinums collection</h1>
+      <h1>{jewellary.length} platinums collection</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-        {golds.slice(20, 30).map((platinum) => (
+        {jewellary.map((platinum) => (
           <Platinum key={platinum._id} platinum={platinum}></Platinum>
         ))}
       </div>
